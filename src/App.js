49ea140b915556930b5096f7c6afd51e20cd6518
Mamzky <auliya77@gmail.com>
2023-01-14 14:41:37 +0700

    call api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [listPost, setPost] = useState(null)
+
+  useEffect(() => {
+
+    try {
+
+      axios.get('https://dummyjson.com/posts').then(
+        (response) => {
+          setPost(response?.data?.posts)
+        },
+        (err) => {
+          console.log('ERROR ==> ', err)
+        }
+      )
+
+    } catch (error) {
+      console.log('CATCH ERROR => ', error)
+    }
+
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Hello world!!
+          POST
         </p>
-        <p>
-          Welcome to mamzky porto
-        </p>
+        {listPost?.map((item) => {
+          return (
+            <div style={{marginBottom: 18}}>
+              <p style={{fontSize: 20, fontStyle:'italic'}}>
+                {item?.title}
+              </p>
+              <p style={{fontSize: 12}}>
+                {item?.body}
+              </p>
+            </div>
+          )
+        })}
+
         <a
           className="App-link"
           href="https://reactjs.org"
