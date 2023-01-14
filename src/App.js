aaa740e5980a1a88e53b2c6953277f0c3857594d
Mamzky <auliya77@gmail.com>
2023-01-14 12:54:07 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to mamzkyporto
+          Hello world!!
+        </p>
+        <p>
+          Welcome to mamzky porto
         </p>
         <a
           className="App-link"
